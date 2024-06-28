16a2ffec0191b2ff0ba11780b9622729f47a3820
sanyavertolet <lxnfrolov@gmail.com>
2024-06-28 17:22:43 +0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("multiplatform") version "2.0.0" apply false
+    id("io.ktor.plugin") version "2.3.12" apply false
+}
+
+group = "com.sanyavertolet.statics"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
